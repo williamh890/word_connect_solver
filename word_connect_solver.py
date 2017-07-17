935846250479224fed712ad9ba99c04bad356ebc
William Horn <wbhorn@alaska.edu>
2017-07-16 19:22:30 -0800

    Still seems to be miss matching
    Need to add suport for having words with multiple of the same letters.
    Also need to fix what seems to be just parts of words getting matched.

--- word_connect_solver.py
+++ word_connect_solver.py
@@ -1,13 +1,9 @@
 import re
 import sys
 
-words = ""
-
-with open("words.txt", "r+") as words_file:
-    words = words_file.read()
-
 
 def remove_dup_letter_words(words):
+    words = list(words)
     for word in reversed(words):
         unique_word = set(list(word))
         #print(len(unique_word), len(word))
@@ -17,18 +13,65 @@ def remove_dup_letter_words(words):
     return words
 
 
-def word_connect_solver(letters):
-    expression = r"\n([" + re.escape(letters) + r"]{2,5})\n"
+def word_connect_solver(letters, words):
+    expression = r"^([" + re.escape(letters) + r"]{3,6})$"
 
-    possible_words = list(set(re.findall(expression, words)))
+    pattern = re.compile(expression)
+
+    possible_words = set()
+    for word in words:
+        if pattern.match(word):
+            possible_words.add(word)
 
     possible_words = remove_dup_letter_words(possible_words)
 
     return sorted(list(set(possible_words)), key=lambda k: len(k))
 
+
+def load_full_dictionary():
+    print("Loading in words...")
+    with open("words_all.txt", 'r') as dictionary:
+        words = dictionary.read().split()
+    print("Done loading words.\n")
+
+    return words
+
+
+def run():
+    print("|-------- Word connect solver --------|")
+    print("|-------- Type 'QUIT' to exit --------|\n")
+
+    words = load_full_dictionary()
+
+    while True:
+        try:
+            letters = input("Enter letters: ")
+        except:
+            print("Error reading letters")
+            continue
+
+        if letters == "QUIT":
+            break
+        else:
+            possible_words = word_connect_solver(letters, words)
+
+        print("\nFound words for letters [{}]:".format(letters))
+
+        to_print = ""
+        for w in possible_words:
+            to_print += w + ', '
+
+        print(to_print[:-1] + '\n')
+    exit()
+
 if __name__ == "__main__":
-    letters = "deb"
+    try:
+        letters = sys.argv[1]
+    except:
+        run()
 
-    possible_words = word_connect_solver(sys.argv[1])
+    with open("words.txt", "r+") as words_file:
+        words = words_file.read().split()
 
+    possible_words = word_connect_solver(sys.argv[1], words)
     print(possible_words)
